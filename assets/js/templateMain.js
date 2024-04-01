5e0beaaa59246faabf20cd6b45dd88cb19d91c28
Nick Wrobel <nickwrobel2@gmail.com>
2024-04-01 15:03:08 -0400

    changes thus far

--- assets/js/templateMain.js
+++ assets/js/templateMain.js
@@ -93,25 +93,25 @@
   /*--/ End Scrolling nav /--*/
 
   /*--/ Navbar Menu Reduce /--*/
-  $(window).trigger('scroll');
-  $(window).on('scroll', function() {
-    var pixels = 50;
-    var top = 1200;
-    if ($(window).scrollTop() > pixels) {
-      $('.navbar-expand-md').addClass('navbar-reduce');
-      $('.navbar-expand-md').removeClass('navbar-trans');
-    } else {
-      if (!$('#navbarDefault').hasClass('show')) {
-        $('.navbar-expand-md').removeClass('navbar-reduce');
-      }
-      $('.navbar-expand-md').addClass('navbar-trans');
-    }
-    if ($(window).scrollTop() > top) {
-      $('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
-    } else {
-      $('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
-    }
-  });
+  // $(window).trigger('scroll');
+  // $(window).on('scroll', function() {
+  //   var pixels = 50;
+  //   var top = 1200;
+  //   if ($(window).scrollTop() > pixels) {
+  //     $('.navbar-expand-md').addClass('navbar-reduce');
+  //     $('.navbar-expand-md').removeClass('navbar-trans');
+  //   } else {
+  //     if (!$('#navbarDefault').hasClass('show')) {
+  //       $('.navbar-expand-md').removeClass('navbar-reduce');
+  //     }
+  //     $('.navbar-expand-md').addClass('navbar-trans');
+  //   }
+  //   if ($(window).scrollTop() > top) {
+  //     $('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
+  //   } else {
+  //     $('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
+  //   }
+  // });
 
   /*--/ Star Typed /--*/
   if ($('.text-slider').length == 1) {
